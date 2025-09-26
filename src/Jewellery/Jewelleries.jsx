aeb774a983ca66eb7f775d917462420d60ee73ab
AdeyemiCode <abdulquadrimahmud06@gmail.com>
2025-09-26 16:00:49 +0100

    product details link added the each product on home page

--- src/Jewellery/Jewelleries.jsx
+++ src/Jewellery/Jewelleries.jsx
@@ -51,8 +51,9 @@ export default function Jewelleries() {
               ))
             : products.map((product) => (
                 <Box key={product._id} borderWidth="1px" borderRadius="md" overflow="hidden" bg="white" _hover={{ shadow: 'md' }} transition="all 0.3s">
-                  <Image src={product.image?.[0]} alt={product.name} height="150px" width="100%" objectFit="cover" />
-                  <Box p={3}>
+                  <Link to={`/product-details/${product?._id}`}>
+                    <Image src={product.image?.[0]} alt={product.name} height="150px" width="100%" objectFit="cover" />
+                  </Link>                  <Box p={3}>
                     <Text fontSize="14px" noOfLines={1}>{product.name}</Text>
                     <Flex justify={'space-between'} align={'center'}>
                       <Badge color="pink.600" fontSize={'16px'}>₦{product.price.toLocaleString()}.00</Badge>
--- src/components/Top_Deals/TopDeals.jsx
+++ src/components/Top_Deals/TopDeals.jsx
@@ -51,7 +51,9 @@ export default function TopDeals() {
               ))
             : products.map((product) => (
                 <Box key={product._id} borderWidth="1px" borderRadius="md" overflow="hidden" bg="white" _hover={{ shadow: 'md' }} transition="all 0.3s">
-                  <Image src={product.image?.[0]} alt={product.name} height="150px" width="100%" objectFit="cover" />
+                  <Link to={`/product-details/${product?._id}`}>
+                    <Image src={product.image?.[0]} alt={product.name} height="150px" width="100%" objectFit="cover" />
+                  </Link>
                   <Box p={3}>
                     <Text fontSize="14px" noOfLines={1}>{product.name}</Text>
                     <Flex justify={'space-between'} align={'center'}>
